d217c04b5c71ca70c827eedaca037e65e1ef0ce0
Patryk Balecki <patrykbalecki@Patryks-MBP.station>
2024-08-11 09:14:58 +0100

    populating base.html and app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
