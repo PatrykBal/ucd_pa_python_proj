5ac6870204e9c3807a4ddf76d26919085038d3e4
Patryk Balecki <patrykbalecki@Patryks-MBP.station>
2024-08-12 19:31:08 +0100

    finished login form using python

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
 
 @app.route("/")
 def index():
@@ -10,13 +14,37 @@ def index():
 def blog():
     return render_template("blog.html")
 
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
 @app.route("/login")
-def login():
+def login_page():
     return render_template("login.html")
 
-@app.route("/base")
-def base():
-    return render_template("base.html")
+database = {'Patryk': '123',
+            'Balecki': 'xyz', 
+            'Jony': 'abc', 
+            'Tony': 'pqr',
+}
+
+@app.route('/form_login', methods=['POST', 'GET'])
+def handle_login():
+    name1 = request.form['username']
+    pwd = request.form['password']
+    if name1 not in database:
+        return render_template('login.html', 
+                               info='Invalid User!')
+    else:
+        if database[name1] != pwd:
+            return render_template('login.html', 
+                                   info='Invalid Password!')
+        else:
+            return render_template('logout.html',
+                                   name=name1)
+ 
+
+
 
 
 if __name__ == "__main__":
